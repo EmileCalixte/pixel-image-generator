1045b5b6ab32a9e4b0ba613c1198acc48b11cc72
Emile Calixte <calixte.emile@gmail.com>
2022-06-13 23:29:15 +0200

    Working v1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import ImageGenerator from "./ImageGenerator";
 
 const App = () => {
@@ -8,6 +8,27 @@ const App = () => {
     const [imageWidth, setImageWidth] = useState(16);
     const [imageHeight, setImageHeight] = useState(16);
     const [seed, setSeed] = useState(BigInt(0));
+
+    const maxSeed = useMemo(() => {
+        const maxSeedBinaryLength = imageWidth * imageHeight;
+
+        const maxSeedBits = [];
+
+        for (let i = 0; i < maxSeedBinaryLength; ++i) {
+            maxSeedBits.push(1);
+        }
+
+        return BigInt("0b" + maxSeedBits.join(''));
+    }, [imageWidth, imageHeight]);
+
+    // The seed passed to the ImageGenerator will never be greater than the max seed
+    const generatorSeed = useMemo(() => {
+        if (seed > maxSeed) {
+            return maxSeed;
+        }
+
+        return seed;
+    }, [seed, maxSeed]);
 
     const onImageWidthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const newValue = e.target.value === "" ? 1 : parseInt(e.target.value);
@@ -26,6 +47,36 @@ const App = () => {
             setSeed(BigInt(e.target.value));
         } catch (e) {}
     }
+
+    const incrementSeed = () => {
+        setSeed(seed + BigInt(1));
+    }
+
+    const decrementSeed = () => {
+        let newSeed = seed - BigInt(1);
+
+        if (newSeed < BigInt(0)) {
+            newSeed = BigInt(0);
+        }
+
+        setSeed(newSeed);
+    }
+
+    const generateRandomSeed = useCallback(() => {
+        const maxSeedBinaryLength = imageWidth * imageHeight;
+
+        const seedBits = [];
+
+        for (let i = 0; i < maxSeedBinaryLength; ++i) {
+            seedBits.push(Math.round(Math.random()));
+        }
+
+        setSeed(BigInt("0b" + seedBits.join('')));
+    }, [imageWidth, imageHeight]);
+
+    const setSeedToMaxSeed = useCallback(() => {
+        setSeed(maxSeed);
+    }, [maxSeed]);
 
     return (
         <div className="App">
@@ -48,11 +99,18 @@ const App = () => {
             </div>
             <div className="image-seed-input-container">
                 <label>
-                    Seed<br/>
+                    Seed
+                    <button onClick={decrementSeed}>-</button>
+                    <button onClick={incrementSeed}>+</button>
+                    &nbsp;
+                    <button onClick={generateRandomSeed}>Random</button>
+                    &nbsp;
+                    <button onClick={setSeedToMaxSeed}>Max</button>
+                    <br/>
                     <textarea value={seed.toString()} onInput={onSeedUpdate}/>
                 </label>
             </div>
-            <ImageGenerator imageWidth={imageWidth} imageHeight={imageHeight} seed={seed} />
+            <ImageGenerator imageWidth={imageWidth} imageHeight={imageHeight} seed={generatorSeed} />
         </div>
     );
 }
--- src/components/ImageGenerator.tsx
+++ src/components/ImageGenerator.tsx
@@ -1,15 +1,54 @@
+import {useCallback, useEffect, useMemo, useRef} from "react";
+
 interface Props {
-    imageWidth: number,
-    imageHeight: number,
-    seed: BigInt
+    imageWidth: number;
+    imageHeight: number;
+    seed: BigInt;
 }
 
 const ImageGenerator = ({imageWidth, imageHeight, seed}: Props) => {
+    const canvasRef = useRef(null);
+
+    const binarySeed = useMemo(() => {
+        return seed.toString(2).padStart(imageWidth * imageHeight, "0").split('').reverse().join('');
+    }, [imageHeight, imageWidth, seed])
+
+    const drawImage = useCallback((ctx: CanvasRenderingContext2D) => {
+        const imageData = ctx.createImageData(imageWidth, imageHeight);
+
+        for (let i = 0; i < imageData.data.length; i += 4) {
+            const value = binarySeed.charAt(Math.floor(i/4)) === '0' ? 0 : 255;
+
+            for (let j = 0; j < 3; ++j) {
+                imageData.data[i+j] = value;
+            }
+
+            imageData.data[i+3] = 255; // Always full alpha
+        }
+
+        ctx.putImageData(imageData, 0, 0);
+    }, [imageWidth, imageHeight, binarySeed]);
+
+    useEffect(() => {
+        if (canvasRef.current === null) {
+            return;
+        }
+
+        const canvas: HTMLCanvasElement = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+
+        if (ctx === null) {
+            return;
+        }
+
+        drawImage(ctx);
+    }, [seed, drawImage])
+
     return (
         <div className="image-generator">
-            <p>Image width: {imageWidth}</p>
-            <p>Image height: {imageHeight}</p>
-            <p>Seed: {seed.toString()}</p>
+            <div className="canvas-container">
+                <canvas ref={canvasRef} width={imageWidth} height={imageHeight} />
+            </div>
         </div>
     );
 }
